e88b605f1130862210601f345c9ba2a528d6f656
Josiah Denton <jfranklindenton@gmail.com>
2024-11-20 16:10:16 -0500

    chore: start at c-a for marks

--- lua/plugins/marks.lua
+++ lua/plugins/marks.lua
@@ -13,10 +13,10 @@ return {
             { "<leader>fa", "<cmd>Grapple toggle<cr>", desc = "Tag a file" },
             { "<c-f>", "<cmd>Grapple toggle_tags<cr>", desc = "Toggle tags menu" },
 
-            { "<c-r>", "<cmd>Grapple select index=1<cr>", desc = "Select first tag" },
-            { "<c-s>", "<cmd>Grapple select index=2<cr>", desc = "Select second tag" },
-            { "<c-t>", "<cmd>Grapple select index=3<cr>", desc = "Select third tag" },
-            { "<c-g>", "<cmd>Grapple select index=4<cr>", desc = "Select fourth tag" },
+            { "<c-a>", "<cmd>Grapple select index=1<cr>", desc = "Select first tag" },
+            { "<c-r>", "<cmd>Grapple select index=2<cr>", desc = "Select second tag" },
+            { "<c-s>", "<cmd>Grapple select index=3<cr>", desc = "Select third tag" },
+            { "<c-t>", "<cmd>Grapple select index=4<cr>", desc = "Select fourth tag" },
 
             { "<c-s-n>", "<cmd>Grapple cycle_tags next<cr>", desc = "Go to next tag" },
             { "<c-s-p>", "<cmd>Grapple cycle_tags prev<cr>", desc = "Go to previous tag" },
